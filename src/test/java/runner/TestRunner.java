e811ad17d0a9499a3ad9faa2b0d757b674614677
Raghuram <2473903@cognizant.com>
2026-04-22 09:14:57 +0530

    feat: added dry run to the test runner

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,11 +1,15 @@
 package runner;
 
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-
+@RunWith(Cucumber.class) 
 @CucumberOptions(
         features = {"src/test/resources/features"},
         glue = {"stepdefinitions"},
+        dryRun= true,
         plugin = {
                 "pretty",
                 "html:target/cucumber-reports.html",
